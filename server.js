499eac84a5c51d4f6c58a6ff412a4b125c385bb9
Brooks Garrett <brooks+git@brooksgarrett.com>
2017-02-21 14:16:26 -0500

    Completed challenge

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const PORT = process.env.PORT || 3000;
 
 var app = express();
 
+app.get('/api/whoami', (req, res) => {
+    console.log(JSON.stringify(req.headers, undefined, 2));
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    var language = req.headers['accept-language'].match(/^[^,]+/)[0];
+    var os = req.headers['user-agent'].match(/^\S+ \(([^)]+)\)/)[1];
+
+    res.send({
+        ipaddress: ip,
+        language: language,
+        software: os
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
